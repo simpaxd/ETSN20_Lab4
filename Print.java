d61cd0488a8d0ce2914aac52f66d47f6bd99bc80
simon Thuresson <simpaxd@gmail.om>
2024-12-01 11:26:38 +0100

    Added feature to print all lines with pattern

--- Print.java
+++ Print.java
@@ -1,6 +1,21 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 public class Print {
     public static void main(String[] args) {
-        System.out.println("Hello World");   
-        System.out.println("Hello World");   
+        String arg1 = "search";
+        String pattern = "cat";
+        String file = "test.txt";
+
+        try {
+            Files.readAllLines(Paths.get(file)).forEach(line -> {
+                if (line.contains(pattern)) {
+                    System.out.println(line);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
