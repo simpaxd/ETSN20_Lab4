861e80164f2c851efdb6267cc878f5215ab98e95
simon Thuresson <simpaxd@gmail.om>
2024-12-01 11:28:11 +0100

    Removed using arrow function

--- Print.java
+++ Print.java
@@ -1,5 +1,6 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.io.IOException;
 
 public class Print {
@@ -9,11 +10,12 @@ public class Print {
         String file = "test.txt";
 
         try {
-            Files.readAllLines(Paths.get(file)).forEach(line -> {
+            List<String> lines = Files.readAllLines(Paths.get(file));
+            for (String line : lines) {
                 if (line.contains(pattern)) {
                     System.out.println(line);
                 }
-            });
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
